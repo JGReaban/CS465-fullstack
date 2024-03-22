7cd46d2cd589b907181c2247c10847ba742e994e
Jeremy Reaban <trancejeremy@yahoo.com>
2024-03-21 23:54:37 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+//var trips = [{"name": "Test"},{"image" : "reef1.jpg"},{"description" : "Some French"}]
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
 /* Get Travel Page */
 const travel = (req,res) =>{
-    res.render('travel', {title: "Travlr Getaways"});
+    res.render('travel', {title: "Travlr Getaways",trips});
 };
 
 module.exports = {
